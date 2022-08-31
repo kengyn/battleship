1c1fdbd510933b02f57e4dab1aceaba4d3fea1c5
Ken <knetngyn@gmail.com>
2022-08-31 17:52:42 -0500

    Create ship factory function

---
+++ src/__tests__/ship.test.js
@@ -0,0 +1,24 @@
+import ship from "../ship";
+
+describe("ship functions", () => {
+  let newShip;
+  beforeEach(() => {
+    newShip = ship(3);
+  });
+
+  test("has length", () => {
+    expect(newShip.getLength).toBe(3);
+  });
+
+  test("can be hit", () => {
+    newShip.hit(2);
+    expect(newShip.shipArr[2].hit).toEqual(true);
+  });
+
+  test("can be sunk", () => {
+    newShip.hit(0);
+    newShip.hit(1);
+    newShip.hit(2);
+    expect(newShip.isSunk()).toBe(true);
+  });
+});
---
+++ src/ship.js
@@ -0,0 +1,23 @@
+const ship = (size) => {
+  const shipArr = [];
+  for (let i = 0; i < size; i++) {
+    shipArr.push({ hit: false });
+  }
+
+  const getLength = shipArr.length;
+
+  const hit = (pos) => {
+    shipArr[pos].hit = true;
+    return shipArr;
+  };
+
+  const isSunk = () => {
+    if (shipArr.every((x) => x.hit == true)) {
+      return true;
+    }
+  };
+
+  return { shipArr, getLength, hit, isSunk };
+};
+
+export default ship;
